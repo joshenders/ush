e582cf2c41832c514033ed0883ad9cd46e782b08
Josh Enders <josh.enders@gmail.com>
2012-07-27 00:38:00 -0700

    sperging

--- ush.c
+++ ush.c
@@ -10,13 +10,14 @@ int main() {
 
     system("clear");
 
-    while(1) {
+    while (1) {
         for (i = 0; i < 4; i++) {
             system("tput cup 0 0"); // move cursor to top left of screen
             printf("%c\t", spinChar[i]);
-            printf("Connected since %s", ctime(&connected));
+            // ctime() has a \n embedded in the output, no need to flush
+            printf("Connected since %s", ctime(&connected));             
             sleep(1);
         }
     }
-    return(0);
+    return 0;
 }
